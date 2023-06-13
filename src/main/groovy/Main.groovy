aaa4962d382ebe943f15a4397d1d961c7f709c29
Paul King <paulk@asert.com.au>
2023-06-13 15:55:26 +1000

    add ginq example

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,15 +1,17 @@
 import groovy.sql.Sql
+import java.util.stream.Collectors
 
 Sql.withInstance('jdbc:duckdb:', 'org.duckdb.DuckDBDriver') { sql ->
-    def rows = sql.rows("""SELECT Country,
+    println "Country  Riders                                  Places    Count"
+    sql.eachRow("""SELECT Country,
         rpad(string_agg(Rider, ', '), 40, ' ') as Riders,
         rpad(string_agg(Place, ', '), 10, ' ') as Places,
         bar(count(Country), 0, 4, 30) as Count
-        FROM 'topten.csv' GROUP BY Country""")
-    println "Country  Riders                                  Places    Count"
-    rows.each { row -> row.with {
-        println "$Country       $Riders$Places$Count"
-    }}
+        FROM 'topten.csv' GROUP BY Country""") { row ->
+        row.with {
+            println "$Country       $Riders$Places$Count"
+        }
+    }
 }
 
 /*
@@ -21,4 +23,35 @@ Country  Riders                                  Places    Count
 🇿🇦       LOUIS DU BOUISSON MEINTJES              7         ███████▌
 🇳🇴       TORSTEIN TRÆEN                          8         ███████▌
 🇪🇸       CARLOS RODRIGUEZ CANO                   9         ███████▌
- */
+*/
+
+var f = 'topten.csv' as File
+var lines = f.readLines()*.split(',')
+var cols = lines[0].size()
+var rows = lines[1..-1].collect{row ->
+    (0..<cols).collectEntries{ col -> [lines[0][col], row[col]] }}
+var commaDelimited = Collectors.joining(', ')
+var aggRiders = { it.stream().map(rec -> rec.r.Rider).collect(commaDelimited) }
+var aggPlaces = { it.stream().map(rec -> rec.r.Place).collect(commaDelimited) }
+println GQ {
+    from r in rows
+    groupby r.Country
+    select r.Country,
+        agg(aggRiders(_g)) as Riders,
+        agg(aggPlaces(_g)) as Places,
+        '██' * count(r.Country) as Count
+}
+
+/*
++---------+--------------------------------------+---------+--------+
+| Country | Riders                               | Places  | Count  |
++---------+--------------------------------------+---------+--------+
+| 🇦🇺    | BEN O'CONNOR, JAI HINDLEY, JACK HAIG | 3, 4, 5 | ██████ |
+| 🇩🇰    | JONAS VINGEGAARD                     | 1       | ██     |
+| 🇳🇴    | TORSTEIN TRÆEN                       | 8       | ██     |
+| 🇿🇦    | LOUIS DU BOUISSON MEINTJES           | 7       | ██     |
+| 🇬🇧    | ADAM YATES                           | 2       | ██     |
+| 🇪🇸    | CARLOS RODRIGUEZ CANO                | 9       | ██     |
+| 🇫🇷    | GUILLAUME MARTIN, JULIAN ALAPHILIPPE | 6, 10   | ████   |
++---------+--------------------------------------+---------+--------+
+*/
